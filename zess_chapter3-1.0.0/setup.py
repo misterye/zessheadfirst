1cb3ffa8cf52104f861b4bb37d8f33eceaba134c
zess1982 <zess1982@gmail.com>
2016-05-16 21:38:16 +0800

    add code of headfirst python chapter3

---
+++ zess_chapter3-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'zess_chapter3',
+    version = '1.0.0',
+    py_modules = ['zess_chapter3'],
+    author = 'zess',
+    author_email = 'zess1982@126.com',
+    url = 'http://satellite.sinaapp.com/',
+    description = 'Manipulate with files.',
+    )
