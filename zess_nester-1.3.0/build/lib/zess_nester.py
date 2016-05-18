9bb09efb9a9cc2dea78337c10b251a1fcb5fb7a6
zess1982 <zess1982@gmail.com>
2016-05-18 10:40:53 +0800

    add headfist zess_nester printer module

--- zess_nester-1.3.0/build/lib/zess_nester.py
+++ zess_nester-1.3.0/build/lib/zess_nester.py
@@ -1,8 +1,7 @@
-import sys
 """ This is the "nester.py" module and it provides one function called print_lol()
     which prints lists that may or may not include nested lists. """
 
-def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
+def print_lol(the_list, indent=False, level=0):
     """ This function takes one positional argument called "the_list", which
          is any Python list (of - possibly - nested lists). Each data item in the
          provided list is (recursively) printed to the screen on it's own line.
@@ -11,9 +10,9 @@ def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
 
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, indent, level+1, fh)
+            print_lol(each_item, indent, level+1)
         else:
             if indent:
                 for tab_stop in range(level):
-                    print("\t", end='', file=fh)
-            print(each_item, file=fh)
+                    print("\t", end='')
+            print(each_item)
